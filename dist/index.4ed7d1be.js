746b280704b0fc96e19984bb73acfccddd18fc43
ercrrc <134560333+ercrrc@users.noreply.github.com>
2023-12-03 16:43:42 -0300

    projeto finalizado

---
+++ dist/index.4ed7d1be.js
@@ -0,0 +1,2 @@
+AOS.init();const e=new Date("Sep 09, 2024 20:00:00").getTime(),t=setInterval(function(){let n=e-new Date().getTime();document.getElementById("contador").innerHTML=`${Math.floor(n/864e5)}d ${Math.floor(n%864e5/36e5)}h ${Math.floor(n%36e5/6e4)}m ${Math.floor(n%6e4/1e3)}s`,n<0&&(clearInterval(t),document.getElementById("contador").innerHTML="Evento Expirado")},1e3);
+//# sourceMappingURL=index.4ed7d1be.js.map
